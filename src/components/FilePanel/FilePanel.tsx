f6437b38091b64e3e05e98f69edecb5585b50f1d
Jelle Overbeek <jelle@MacBook-Pro-van-Jelle.local>
2019-10-06 15:54:16 +0200

    added react dropzone

--- src/components/FilePanel/FilePanel.tsx
+++ src/components/FilePanel/FilePanel.tsx
@@ -2,8 +2,19 @@ import React, { Component } from "react";
 import "./FilePanel.css";
 import File from "../File/File";
 import TopBar from "../TopBar/TopBar";
+import Dropzone from "react-dropzone";
 
 export class FilePanel extends Component {
+  handleFiles(file: object) {
+    console.log(file);
+
+    // sharp(file[0].path)
+    //   .resize(320, 240)
+    //   .toFile("output.webp", (err, info) => {
+    //     console.log(err, info);
+    //   });
+  }
+
   render() {
     return (
       <aside className="file-panel">
@@ -18,11 +29,20 @@ export class FilePanel extends Component {
             <File name="image-dfasdf.png" />
           </ul>
           <div className="instructions">
-            <h2>Drop images</h2>
-            <p className="paragraph--small">
-              Processing starts as soon as your drop an image. Supported formats
-              are PNG and JPG
-            </p>
+            <Dropzone onDrop={acceptedFiles => this.handleFiles(acceptedFiles)}>
+              {({ getRootProps, getInputProps }) => (
+                <section>
+                  <div {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <h2>Drop images</h2>
+                    <p className="paragraph--small">
+                      Processing starts as soon as your drop an image. Supported
+                      formats are PNG and JPG
+                    </p>
+                  </div>
+                </section>
+              )}
+            </Dropzone>
           </div>
         </div>
       </aside>
