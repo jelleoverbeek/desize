4e9116a4e4285e0d70e9a17e7f085d35cb6219ef
Jelle Overbeek <overbeekjelle@gmail.com>
2020-03-04 22:27:58 +0100

    cleanup and name change

--- public/electron.js
+++ public/electron.js
@@ -38,52 +38,6 @@ function createWindow() {
   );
 
   mainWindow.on("closed", () => (mainWindow = null));
-
-  // // Create the Application's main menu
-  // var template = [
-  //   {
-  //     label: "Img Tool",
-  //     submenu: [
-  //       {
-  //         label: "About Img Tool",
-  //         selector: "orderFrontStandardAboutPanel:"
-  //       },
-  //       { type: "separator" },
-  //       {
-  //         label: "Quit",
-  //         accelerator: "Command+Q",
-  //         click: function() {
-  //           app.quit();
-  //         }
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     label: "Edit",
-  //     submenu: [
-  //       { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-  //       { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-  //       { type: "separator" },
-  //       { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-  //       { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-  //       { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-  //       {
-  //         label: "Select All",
-  //         accelerator: "CmdOrCtrl+A",
-  //         selector: "selectAll:"
-  //       },
-  //       {
-  //         label: "Dev tools",
-  //         accelerator: "CmdOrCtrl+J",
-  //         click() {
-  //           mainWindow.webContents.openDevTools();
-  //         }
-  //       }
-  //     ]
-  //   }
-  // ];
-
-  // Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 app.on("ready", createWindow);
