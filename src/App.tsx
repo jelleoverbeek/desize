8a6a6d8ae5912746cb04facca1ce829b7ff60d7a
Jelle Overbeek <overbeekjelle@gmail.com>
2022-01-13 21:04:54 +0100

    refactor app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import React, { Component } from "react";
-import "./App.css";
-import FilePanel from "./components/FilePanel/FilePanel";
-import OptionsPanel from "./components/OptionsPanel/OptionsPanel";
+import React from 'react';
+import './App.css';
+import FilePanel from './components/FilePanel/FilePanel';
+import OptionsPanel from './components/OptionsPanel/OptionsPanel';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <OptionsPanel />
-        <FilePanel />
-      </div>
-    );
-  }
-}
+const App: React.FunctionComponent = (): JSX.Element => (
+  <div className="App">
+    <OptionsPanel />
+    <FilePanel />
+  </div>
+);
 
 export default App;
