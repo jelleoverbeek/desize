c56ef611e9cc7502300900314d6d340bebd30998
Jelle Overbeek <overbeekjelle@gmail.com>
2022-03-14 17:45:16 +0100

    cleanup

--- src/components/FileItem/FileItem.tsx
+++ src/components/FileItem/FileItem.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import IFile from '../../interfaces/IFile.interface';
 import './FileItem.css';
 import Loader from '../Loader/Loader';
